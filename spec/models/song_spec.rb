39a168adc516165102090fad930a9ec89694113c
Rudy Ravelin <rudy@noukod.com>
2019-02-05 20:45:49 +0000

    Automatically backed up by Learn

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,5 @@
 # require 'rails_helper'
-# 
+#
 # RSpec.describe Song, type: :model do
 #   before do
 #     Artist.destroy_all
@@ -7,11 +7,11 @@
 #     @artist = Artist.create!(name: "Daft Punk")
 #     @grid = @artist.songs.create!(title: "The Grid")
 #   end
-# 
+#
 #   it "gets the artist name" do
 #     expect(@grid.artist_name).to eq("Daft Punk")
 #   end
-# 
+#
 #   it "can set the artist via name" do
 #     song = Song.new(title: "Mad World")
 #     song.artist_name = "Tears for Fears"
@@ -19,7 +19,7 @@
 #     expect(song.artist_name).to eq "Tears for Fears"
 #     expect(song.artist.name).to eq "Tears for Fears"
 #   end
-# 
+#
 #   it "finds artist if already exists" do
 #     song = Song.create(title: "Around the World", artist_name: "Daft Punk")
 #     expect(song.artist_name).to eq "Daft Punk"
